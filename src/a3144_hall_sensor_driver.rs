d96a40e133305788d05f9aad559dc4d724f8b69b
Michael Matthews <michaeldylanmatthews@gmail.com>
2021-08-02 22:35:55 -0400

    implemented hall effect sensor

---
+++ src/a3144_hall_sensor_driver.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+use rppal::gpio::{Gpio, InputPin};
+
+pub struct HallSensor {
+    sens_in: InputPin,
+    step_count: u32,
+}
+
+impl HallSensor {
+
+    #[allow(non_snake_case)] //pins should be constant, names reflect that
+    pub fn new(PIN_HALL_IN: u8) -> Result<HallSensor, Box<dyn Error>>{
+        let hall_sensor = HallSensor {
+            sens_in: Gpio::new()?.get(PIN_HALL_IN)?.into_input_pullup(),
+            step_count: 0,
+        };
+        Ok(hall_sensor)
+    }
+
+    pub fn dist_from_home() -> f32 {
+        // math to convert step_count to distance
+        0.0
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,24 @@
 use std::error::Error;
 use std::{thread, time::Duration};
 
-mod l239d_motor_dsriver;
+mod l239d_motor_driver;
 use crate::l239d_motor_driver::Motor;
+
+mod a3144_hall_sensor_driver;
+use crate::a3144_hall_sensor_driver::HallSensor;
 
 //pins connected to L293D
 const PIN_MOTOR_EN: u8 = 13;
 const PIN_MOTOR_1A: u8 = 6;
 const PIN_MOTOR_2A: u8 = 5;
 
+const PIN_HALL_IN: u8 = 26;
+
 fn main() -> Result<(), Box<dyn Error>>  {
 
     let mut motor = Motor::new(PIN_MOTOR_EN, PIN_MOTOR_1A, PIN_MOTOR_2A)?;
-    
-    
-    ctrlc::set_handler(move || {
-        let temp_motor = Motor::new(PIN_MOTOR_EN, PIN_MOTOR_1A, PIN_MOTOR_2A);
-        let mut temp_motor = match temp_motor {
-            Ok(motor) => motor,
-            Err(_e) => {std::process::exit(1);},
-        };
-        temp_motor.off();        
-    })
-    .expect("Error setting Ctrl-C handler"); 
-    
-    
+    let mut encoder = HallSensor::new(PIN_HALL_IN)?;
+
     loop {
         motor.spin_forward();
         
@@ -37,7 +31,7 @@ fn main() -> Result<(), Box<dyn Error>>  {
         println!("spinning right?");
         thread::sleep(Duration::from_secs(1));
 
-        motor.brake();
+        motor.off();
 
         println!("stopping.");
         thread::sleep(Duration::from_secs(1));
